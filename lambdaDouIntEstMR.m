a31e45b8516e5e8226be532d62972faae7dfde3f
Ge-zichuan <45208759+Ge-zichuan@users.noreply.github.com>
2024-02-13 12:59:01 -0800

    Delete lambdaDouIntEstMR.m

--- lambdaDouIntEstMR.m
+++
@@ -1,5 +0,0 @@
-function lambdaDouInt = lambdaDouIntEstMR(parasLower,inputt1,inputg1,xall,zall,deltaall,samplesize,bands,d,group)
-    innerCumulHazard = integral(@(x)lambdaEstMRSpec(parasLower,x,inputg1,xall,zall,deltaall,samplesize,bands,d,group)...
-        ,0,inputt1,'ArrayValued',true);
-    lambdaDouInt = exp(-innerCumulHazard);
-end
