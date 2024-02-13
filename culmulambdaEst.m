9c2b96331311f17ece687d65d64058a6acfa0981
Ge-zichuan <45208759+Ge-zichuan@users.noreply.github.com>
2024-02-13 12:57:05 -0800

    Delete culmulambdaEst.m

--- culmulambdaEst.m
+++
@@ -1,4 +0,0 @@
-function culmulambdaVec = culmulambdaEst(inputt,inputg,zall,aall,deltaall,samplesize,goodIndex,bands)
-% This is the estimation of Lambda, integral of lambda from 0 to t.
-culmulambdaVec = integral(@(x)lambdaEst(x,inputg,zall,aall,deltaall,samplesize,goodIndex,bands),0,inputt,'ArrayValued',true);
-end
