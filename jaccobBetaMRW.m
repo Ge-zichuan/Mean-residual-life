b6e17b26607a0609026386fc86761ac5bfd229e1
Ge-zichuan <45208759+Ge-zichuan@users.noreply.github.com>
2024-11-23 14:26:54 -0800

    Add files via upload

--- jaccobBetaMRW.m
+++ jaccobBetaMRW.m
@@ -1,4 +1,4 @@
-function [stdVal,jaccobVal] = jaccobBetaMRW(parasLower,xall,wall,zdiffallN,zdiffallT,zdiffall,kbzN,kbzT,kbzND,kbzTD...
+function [stdVal,jaccobVal] = jaccobBetaMRW(parasLower,xall,wall,zdiffallN,zdiffallT,zdiffall,kbzN,kbzT...
     ,deltaallN,deltaallT,samplesize,samplesizeN,samplesizeT,groupIndex,p,d,bands)
 % khbetax: K_h(\bb\trans\x)
 % denoVev: \sumi I()K_h()
@@ -18,11 +18,9 @@ bandnh = bands(3,1);
 bandadjnhn = bands(3,2);
 bandadjnhd = bands(3,3);
 bandhe = bands(4,1);
-bandadjen = bands(4,2);
 bandadjed = bands(4,3);
 % calculate \bb\trans\x
 btransx = xall*[eye(d);parasLower]; % paras1 include upper d-by-d block.
-btransxN = btransx; % paras1 include upper d-by-d block.
 btransxT = [btransx(groupIndex==1,:),wall(groupIndex==1,:)]; % paras1 include upper d-by-d block.
 % calculate all \bb\trans\x_i-\bb\trans\x_j (i row j column)
 if (d>1)
@@ -51,7 +49,7 @@ khbetaxN = kernel(btxallN/(banddh*bandadjdhn),kerD,'Epanechnikov')/(banddh*banda
 lambdaDen(:,1) = sum(kbzN.*repmat(deltaallN,1,samplesizeN)'.*khbetaxN./denoVecN,2);
 lambdaDen(groupIndex==1,1) = sum(kbzT.*repmat(deltaallT,1,samplesizeT)'.*khbetaxT./denoVecT,2);
 % calculate kernel values of K\prime(.)
-khprimevecT = kernel2(btxallT/(bandnh*bandadjnhn),kerD,d,'Epanechnikov')/(bandnh*bandadjnhn)^(d+1);
+khprimevecT = kernel2(btxallT/(bandnh*bandadjnhn),kerD,d+1,'Epanechnikov')/(bandnh*bandadjnhn)^(d+2);
 khprimevecN = kernel2(btxallN/(bandnh*bandadjnhn),kerD,d,'Epanechnikov')/(bandnh*bandadjnhn)^(d+1);
 for t = 1:d
     numeVecT(:,:,t) = squeeze(khprimevecT(:,:,t))*(zdiffallT>=0);
@@ -61,28 +59,17 @@ lambdaNum = zeros(samplesize,d);
 khbetaxT = kernel(btxallT/(bandnh*bandadjnhd),kerD,'Epanechnikov')/(bandnh*bandadjnhd)^d;
 khbetaxN = kernel(btxallN/(bandnh*bandadjnhd),kerD,'Epanechnikov')/(bandnh*bandadjnhd)^d;
 for t = 1:d
-    lambdaNum(:,t) = sum(-kbzND.*repmat(deltaallN,1,samplesizeN)'.*squeeze(khprimevecN(:,:,t))./denoVecN,2)...
-        +sum(kbzND.*repmat(deltaallN,1,samplesizeN)'.*khbetaxN.*squeeze(numeVecN(:,:,t))./(denoVecN.^2),2);
-    lambdaNum(groupIndex==1,t) = sum(-kbzTD.*repmat(deltaallT,1,samplesizeT)'.*squeeze(khprimevecT(:,:,t))./denoVecT,2)...
-        +sum(kbzTD.*repmat(deltaallT,1,samplesizeT)'.*khbetaxT.*squeeze(numeVecT(:,:,t))./(denoVecT.^2),2);
+    lambdaNum(:,t) = sum(-kbzN.*repmat(deltaallN,1,samplesizeN)'.*squeeze(khprimevecN(:,:,t))./denoVecN,2)...
+        +sum(kbzN.*repmat(deltaallN,1,samplesizeN)'.*khbetaxN.*squeeze(numeVecN(:,:,t))./(denoVecN.^2),2);
+    lambdaNum(groupIndex==1,t) = sum(-kbzT.*repmat(deltaallT,1,samplesizeT)'.*squeeze(khprimevecT(:,:,t))./denoVecT,2)...
+        +sum(kbzT.*repmat(deltaallT,1,samplesizeT)'.*khbetaxT.*squeeze(numeVecT(:,:,t))./(denoVecT.^2),2);
 end
 lambdaVec = lambdaNum./repmat(lambdaDen,1,d);
-% lambdaVec = exp(btransx)./repmat(sum(exp(btransx),2),1,d);
-% lambdaVec(isnan(lambdaVec)) = 0;
-% calculate nonparametric expectations
-% khbetax = kernel(btxallT/bandhe/bandadjed,3,'norm')/bandhe/bandadjed;
-% expectDenT = sum(khbetax.*(zdiffallT<=0),2)./sum(khbetax,2);
-% khbetaxT = kernel(btxallT/bandhe/bandadjen,3,'norm')/bandhe/bandadjen;
-% khbetax = kernel(btxallN/bandhe/bandadjed,3,'norm')/bandhe/bandadjed;
-% expectDenN = sum(khbetax.*(zdiffallN<=0),2)./sum(khbetax,2);
-% khbetaxN = kernel(btxallN/bandhe/bandadjen,3,'norm')/bandhe/bandadjen;
 khbetax = kernel(btxall/(bandhe*bandadjed),3,'Epanechnikov')/(bandhe*bandadjed)^d;
 expectDen = sum(khbetax.*(zdiffall<=0),2)./sum(khbetax,2);
 khbetax = kernel(btxall/(bandhe*bandadjed),3,'Epanechnikov')/(bandhe*bandadjed)^d;
 for t = 1:(p-d)
     expectVec(:,t) = xall(:,d+t)-sum(khbetax.*repmat(xall(:,d+t),1,samplesize)'.*(zdiffall<=0),2)./sum(khbetax,2)./expectDen;
-%     expectVec(:,t) = xallN(:,d+t)-sum(khbetaxN.*repmat(xallN(:,d+t),1,samplesizeN)'.*(zdiffallN<=0),2)./sum(khbetaxN,2)./expectDenN;
-%     expectVec(groupIndex==1,t) = xallT(:,d+t)-sum(khbetaxT.*repmat(xallT(:,d+t),1,samplesizeT)'.*(zdiffallT<=0),2)./sum(khbetaxT,2)./expectDenT;
 end
 
 for i = 1:d
